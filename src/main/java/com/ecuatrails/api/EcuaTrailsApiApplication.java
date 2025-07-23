e860c4e13e9c7544abe899c3a231deb0f2c47b84
Adrian Acuña <adriansamacu71@gmail.com>
2025-07-22 21:24:39 -0500

    Springboot Project Init

---
+++ src/main/java/com/ecuatrails/api/EcuaTrailsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ecuatrails.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcuaTrailsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcuaTrailsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecuatrails/api/EcuaTrailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecuatrails.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcuaTrailsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
